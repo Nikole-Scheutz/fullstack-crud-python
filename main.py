d658881d0a9830a514ff4d839e80c2114071ba84
Nikole-Scheutz <nikolai@jinxan.se>
2023-09-03 16:33:29 +0200

    add error handling and program run loop

--- main.py
+++ main.py
@@ -2,24 +2,30 @@
 
 class Noter:
     def __init__(self):
-        self.notes_list = [];
+        self.note_list = [];
 
-    note_list = [];
 
     def create(self, contents):
         self.note_list.append(contents);
     
     def read(self, note):
         try:
+            note = int(note);
             print(self.note_list[note]);
         except:
             print("ERROR, NOTE NOT IN LIST");
     
     def update(self, note, contents):
+        note = int(note);
         self.note_list[note] = contents;
     
     def delete(self, note):
+        note = int(note);
         self.note_list.pop(note);
+
+    def list(self):
+        for i, note in enumerate(self.note_list):
+            print("-", i, note);
 
 noter = Noter();
 
@@ -34,3 +40,38 @@ noter.read(0);
 
 noter.update(0, "HELLO");
 noter.read(0);
+
+noter.delete(0);
+
+while True:
+    user_input = input();
+    match user_input:
+        case "create":
+            note = input("What to save?\n");
+            noter.create(note);
+            pass;
+
+        case "read":
+            note = input("Read which note?\n");
+            note = int(note);
+            if 0 <= note < len(noter.note_list):
+                noter.update(note, "TEST2");
+            noter.read(note);
+            pass;
+
+        case "update":
+            note = input("Edit which note?\n");
+            note = int(note);
+            if 0 <= note < len(noter.note_list):
+                noter.update(note, "TEST2");
+            pass;
+
+        case "delete":
+            note = input("Delete which note?\n");
+            note = int(note);
+            if 0 <= note < len(noter.note_list):
+                noter.delete(note);
+            pass;
+
+        case "list":
+            noter.list();
