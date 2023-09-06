91619c6e6b53cb9ef64f0a1888a0b1de0f9616ec
Nikole-Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
2023-09-06 22:09:08 +0200

    ugly edit_note() functionality

--- backend/repository/repository.py
+++ backend/repository/repository.py
@@ -14,4 +14,19 @@ def get_note(db: Session, note_id: int):
     return db.query(models.Note).filter(models.Note.note_id == note_id).first()
 
 def get_notes(db: Session, user_id: int):
+    assert db.query(models.Note) is not None
     return db.query(models.Note).filter(models.Note.user_id == user_id).all()
+
+def edit_note(db: Session, note_id: int, note: schemas.Note):
+    db.query(models.Note)\
+            .filter(models.Note.note_id == note_id)\
+            .update({
+                models.Note.title: note.title,
+                models.Note.content: note.content,
+                models.Note.user_id: note.user_id,
+                },
+                synchronize_session="evaluate"
+                )
+    db.commit()
+    return db.query(models.Note).filter(models.Note.note_id == note_id).first()
+    
