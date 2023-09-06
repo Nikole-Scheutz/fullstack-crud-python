7ba6a5d0cdc12bf27d752c7be654dff646bcf228
Nikole-Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
2023-09-06 20:10:34 +0200

    move repository files into repository folder for separation

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,8 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.decl_api import MetaData
 
-from . import repository, models, schemas
-from .database import SessionLocal, engine
+from .repository import repository, models, schemas
+from .repository.database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine) # THIS SHOWS AN ERROR, BUT SEEMS TO WORK ANYWAYS
 
@@ -17,12 +17,11 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/note", response_model=schemas.Note)
-def get_note(db: Session = Depends(get_db)):
-    note = repository.get_note(db, 1)
+@app.get("/note/{note_id}", response_model=schemas.Note)
+def get_note(note_id: int, db: Session = Depends(get_db)):
+    note = repository.get_note(db, note_id)
     return note
 
-@app.post("/note/{user_id}", response_model=schemas.Note)
-def create_note(user_id: int, note: schemas.NoteCreate, db: Session = Depends(get_db)):
-    note.user_id = user_id
+@app.post("/note/", response_model=schemas.Note)
+def create_note(note: schemas.NoteCreate, db: Session = Depends(get_db)):
     return repository.create_note(db, note)
