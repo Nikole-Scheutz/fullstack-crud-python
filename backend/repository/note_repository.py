1381bd02627271e6408a4425ec719af696b2e41c
Nikole-Scheutz <112065881+Nikole-Scheutz@users.noreply.github.com>
2023-09-07 14:05:06 +0200

    fix edit_note by fixing dictionary of update method

--- backend/repository/note_repository.py
+++ backend/repository/note_repository.py
@@ -20,9 +20,9 @@ def edit_note(db: Session, note_id: int, note: schemas.Note):
     db.query(models.Note)\
             .filter(models.Note.note_id == note_id)\
             .update({
-                f"{models.Note.title}": note.title,
-                f"{models.Note.content}": note.content,
-                f"{models.Note.user_id}": note.user_id,
+                "title": note.title,
+                "content": note.content,
+                "user_id": note.user_id,
                 },
                 synchronize_session="evaluate"
                 )
